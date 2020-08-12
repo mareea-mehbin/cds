60624e9a60bb0b1141f21edeea33c6d42ba0832c
Eric <ericmiranda7@gmail.com>
2020-08-12 10:35:11 +0530

    display criminals

--- criminals/views.py
+++ criminals/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Criminal
+from django.http import HttpResponse
 
 # Create your views here.
+def display(request):
+    criminals = Criminal.objects.all()
+    return render(request, 'criminals/display.html', {'criminals': criminals})
