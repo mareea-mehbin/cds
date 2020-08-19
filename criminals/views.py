0ecdcc53f35ad0c07642e70000196d32a092f06e
Eric <ericmiranda7@gmail.com>
2020-08-19 15:25:18 +0530

    paginate criminal_view

--- criminals/views.py
+++ criminals/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Criminal
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+
+from .models import Criminal
 
 # Create your views here.
 def search(request):
@@ -8,4 +10,8 @@ def search(request):
         criminals = Criminal.objects.filter(name=request.GET.get('search'))
     else:
         criminals = Criminal.objects.all()
-    return render(request, 'criminals/criminal_view.html', {'criminals': criminals})
+
+    paginator = Paginator(criminals, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'criminals/criminal_view.html', {'criminals': page_obj})
