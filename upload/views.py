bfdc705c42a6d5b52aab266f9d701eb4224fe714
Eric <ericmiranda7@gmail.com>
2020-09-02 14:58:43 +0530

    connect form to db

--- upload/views.py
+++ upload/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UploadCriminalForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def upload_entry(request):
-    form = UploadCriminalForm()
+    if request.method =='GET':
+        form = UploadCriminalForm()
+    else:
+        form = UploadCriminalForm(request.POST)
+        if form.is_valid():
+            new_upload = form.save()
+            return redirect('upload:upload')
     return render(request, 'upload/upload_form.html', {'uploadForm': form})
+
