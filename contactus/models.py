dca9eec6cb244f043dc1dc963165aafb9670d4d5
Eric <ericmiranda7@gmail.com>
2020-09-05 20:43:02 +0530

    make contact form model

--- contactus/models.py
+++ contactus/models.py
@@ -5,6 +5,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=30)
     email = models.CharField(max_length=30, primary_key=True)
     phone = models.IntegerField()
+    message = models.TextField()
 
     def __str__(self):
         return self.name
--- contactus/views.py
+++ contactus/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def contact(request):
+    if (request.method == 'POST'):
+        return HttpResponse("submitted")
     return render(request, 'contactus/contact.html')
