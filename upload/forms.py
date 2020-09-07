7a60f5699ccd143553a726d2f62720faa39c228d
Eric <ericmiranda7@gmail.com>
2020-09-07 13:14:24 +0530

    start styling form

--- upload/forms.py
+++ upload/forms.py
@@ -5,3 +5,8 @@ class UploadCriminalForm(forms.ModelForm):
     class Meta:
         model = UploadCriminal
         fields = ['name', 'age', 'physical_description', 'date', 'state', 'city', 'crime_type', 'arresting_agency', 'photo']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': "Enter your name", 'class': 'form-control'}),
+            'age': forms.TextInput(attrs={'placeholder': "Enter Criminal's age", 'class': 'form-control'})
+        }
