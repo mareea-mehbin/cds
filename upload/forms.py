87ac0a8ffd0d7076a5ddbeefa227335fc9bcf904
Eric <ericmiranda7@gmail.com>
2020-09-09 11:05:52 +0530

    style upload form

--- upload/forms.py
+++ upload/forms.py
@@ -4,9 +4,18 @@ from .models import UploadCriminal, States, Cities
 class UploadCriminalForm(forms.ModelForm):
     class Meta:
         model = UploadCriminal
-        fields = ['name', 'age', 'physical_description', 'date', 'state', 'city', 'crime_type', 'arresting_agency', 'photo']
+        fields = ['name', 'age', 'physical_description', 'date',
+        'state', 'city', 'crime_type', 'arresting_agency', 'photo']
 
         widgets = {
             'name': forms.TextInput(attrs={'placeholder': "Enter your name", 'class': 'form-control'}),
-            'age': forms.TextInput(attrs={'placeholder': "Enter Criminal's age", 'class': 'form-control'})
+            'age': forms.TextInput(attrs={'placeholder': "Enter Criminal's age", 'class': 'form-control'}),
+            'physical_description': forms.TextInput(attrs={'placeholder': 'Enter physical description of criminal', 'type': 'textarea', 'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'state': forms.Select(attrs={'name': 'state', 'id': 'id_state', 'class': 'form-control'}),
+            'city': forms.Select(attrs={'id': 'id_city', 'class': 'form-control chained-fk', 'name': 'city', 'data-chainfield': 'state', 'data-url': '/chaining/filter/upload/Cities/state/upload/UploadCriminal/city', 'data-value': 'null', 'data-auto_choose': 'true', 'data-empty_label': 'Select City'}),
+            'crime_type': forms.TextInput(attrs={'placeholder': 'Type of Crime', 'class': 'form-control'}),
+            'arresting_agency': forms.TextInput(attrs={'placeholder': 'Arresting Agency', 'class': 'form-control'}),
+            'photo': forms.FileInput(attrs={'accept': 'image/*', 'name': 'photo', 'class': 'form-control', 'style': 'border: none;'})
         }
+""" <input type="file" name="photo" accept="image/*" id="id_photo"> """
