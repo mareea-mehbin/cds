04e61e332a0a32d2c8dbcf6562adec3e37398612
Eric <ericmiranda7@gmail.com>
2020-09-10 17:46:40 +0530

    shift criminal to listview

--- criminals/views.py
+++ criminals/views.py
@@ -3,10 +3,28 @@ from django.core.paginator import Paginator
 from .models import Criminal
 from .forms import AdvancedSearch
 from django.views import View
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView, ListView
 
-class CriminalsView(View):
+class CriminalsView(ListView):
+    template_name = 'criminals/criminals.html'
+    form_class = AdvancedSearch
+    model = Criminal
+    context_object_name = 'criminals'
+
+    def get_queryset(self):
+        name = self.kwargs.get('name', None)
+
+        form = self.form_class(self.request.GET)
+
+        if form.is_valid():
+            return self.model.objects.filter(name__contains=form.cleaned_data['name'])
+        return self.model.objects.all()
+
     def get(self, request):
+        form = AdvancedSearch()
+        criminals = Criminal.objects.all()
+
+    """ def get(self, request):
         # If advanced search
         if (request.GET.get('name')):
                 form = AdvancedSearch(request.GET)
@@ -24,11 +42,8 @@ class CriminalsView(View):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
-        return render(request, 'criminals/criminals.html', {'criminals': page_obj, 'advancedSearch': form})
+        return render(request, 'criminals/criminals.html', {'criminals': page_obj, 'advancedSearch': form}) """
 
 class CriminalDetailView(DetailView):
     model = Criminal
-
-    def get(self, request, pk):
-        criminal = self.model.objects.get(pk=pk)
-        return render(request, 'criminals/criminal_detail.html', {'criminal': criminal})
+    template_name = 'criminals/criminal_detail.html'
