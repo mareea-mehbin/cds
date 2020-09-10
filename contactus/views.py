b667d5158e4a64b452f18c6fd6135584df55acce
Eric <ericmiranda7@gmail.com>
2020-09-10 16:21:42 +0530

    use django messages for contact submission

--- contactus/views.py
+++ contactus/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.urls import reverse
 from django.views import View
+from django.contrib import messages
 
 # Create your views here.
 class ContactFormView(View):
@@ -13,6 +14,7 @@ class ContactFormView(View):
         form = ContactForm(request.POST)
         if (form.is_valid()):
             new_message = form.save()
+            messages.success(request, 'Thank you for contacting us ! We will be with you shortly')
             return redirect('contactus:success')
         else:
             return render(request, 'contactus/contact.html', {'contactForm': form})
