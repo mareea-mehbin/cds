68149fa27f4a66f7e4416354c731d5ead5c92e1c
Eric <ericmiranda7@gmail.com>
2020-09-12 01:18:44 +0530

    use templateview for home

--- cds/urls.py
+++ cds/urls.py
@@ -18,11 +18,12 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chaining/', include('smart_selects.urls')),
-    path('', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('upload/', include('upload.urls')),
     path('contact/', include('contactus.urls')),
     path('accounts/', include('accounts.urls'), name='accounts'),
--- cds/views.py
+++ cds/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render, redirect
 from contactus.forms import ContactForm
 from django.urls import reverse
-
-def home(request):
-    return render(request, 'home.html')
