e6ef8296fceaf5808936d4b5fd1e29bac20fc7a7
Eric <ericmiranda7@gmail.com>
2020-09-11 23:39:05 +0530

    fix contact submit redirect

--- contactus/views.py
+++ contactus/views.py
@@ -4,12 +4,14 @@ from django.urls import reverse
 from django.views import View
 from django.contrib import messages
 from django.views.generic import FormView
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
-class ContactView(FormView):
+class ContactView(SuccessMessageMixin, FormView):
     template_name = 'contactus/contact.html'
     form_class = ContactForm
-    success_url = '/success/'
+    success_url = 'success/'
+    success_message = "Submitted, Thank You !"
 
     def form_valid(self, form):
         new_message = form.save()
